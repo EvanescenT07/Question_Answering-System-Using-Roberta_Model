0c07f7b4a4f56402ef01a814b92a24a1a6f9b230
EvanescenT07 <zulfikar.ahmad12@yahoo.com>
2024-08-23 14:57:04 +0700

    Update website UI

--- app.py
+++ app.py
@@ -5,21 +5,30 @@ import torch
 # Check if CUDA is available
 use_cuda = torch.cuda.is_available()
 
-model_path = "best_model" 
+model_path = "best_model"
 model = QuestionAnsweringModel("roberta", model_path, use_cuda=use_cuda)
 
 # Streamlit UI
 st.title("Question Answering System")
+st.write("This project is a final submission for a Natural Language Processing (NLP) course. It implements a Question Answering System using the RoBERTa pretrained model and the SQuAD dataset.")
+st.write("---")
+st.write("Instructions:")
+st.write("1. Enter the context in the text area.")
+st.write("2. Enter the question in the text box.")
+st.write("3. Click the 'Answer' button to get the answer to the question.")
 
-context = st.text_area("Enter Context:", height=200)
-question = st.text_input("Enter Question:")
+context = st.text_area(
+    "Enter Context:", placeholder="Add the text you want to use as context here", height=200)
+question = st.text_input(
+    "Enter Question:", placeholder="Type your question about the context")
 
 if st.button("Answer"):
     if context and question:
-        answers, probabilities = model.predict([{"context": context, "qas": [{"question": question, "id": "001"}]}])
+        answers, probabilities = model.predict(
+            [{"context": context, "qas": [{"question": question, "id": "001"}]}])
 
         # Extract the answer from the list and remove the index
-        answer = answers[0]['answer'][0]  
+        answer = answers[0]['answer'][0]
         if answer:
             st.write("**Answer :**\n", answer)
         else:
